52e08a7f8a6b86122368a9cd243572605b8602fb
Sun565a <vg544851@gmail.com>
2025-10-19 22:24:59 -0300

    Add files via upload

---
+++ 2738230.lua
@@ -0,0 +1,6 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(2738230)
+addappid(2738232, 1, "72bfadf09b8dccca337a222427d8be95c327c3601a7bf8748e02d0f9828b5650")

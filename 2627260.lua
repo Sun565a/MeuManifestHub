d3899928dccf37db8dba834df68b39ce08cb03ed
Sun565a <vg544851@gmail.com>
2025-10-21 15:42:50 -0300

    Delete 2627260.lua

--- 2627260.lua
+++
@@ -1,8 +0,0 @@
--- Created by SAI https://github.com/pjy612/SteamManifestCache
--- https://t.me/sai_update
--- https://t.me/sai_notice
-addappid(3322910)
-addappid(3717100)
-addappid(3717110)
-addappid(2627261,0,"1dbbfa4a1302ac713af5d572bd94e7d0708ba581ca184c4124588ff3fdc06481")
-setManifestid(2627261,"4989820315570254152")
